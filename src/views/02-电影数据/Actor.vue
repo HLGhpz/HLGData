27474e36157b7c5a5dcc3a7c7fe3553984c889c8
hlghpz <1090377416@qq.com>
2021-02-17 21:06:14 +0800

    complete the actor.vue

--- src/views/02-电影数据/Actor.vue
+++ src/views/02-电影数据/Actor.vue
@@ -11,6 +11,7 @@ export default {
       startMovie: 0,
       addMovie: 1,
       titleFontSize: 0,
+      timerId: null,
     };
   },
   computed: {
@@ -21,7 +22,7 @@ export default {
   mounted() {
     this.initChart();
     this.getData();
-    // this.dataInterval();
+    this.dataInterval();
     window.addEventListener("resize", this.screenAdapter);
     this.screenAdapter();
   },
@@ -29,9 +30,13 @@ export default {
     initChart() {
       this.chartInstance = this.$echarts.init(this.$refs.chart, "light");
       const initOption = {
+        grid: {
+          left: "10%",
+          right: "30%",
+          top: "10%",
+        },
         xAxis: {
           type: "value",
-          scale: true,
           axisLabel: {
             formatter: (msg) => {
               let strRet = "";
@@ -42,46 +47,51 @@ export default {
             },
             showMinLabel: false,
           },
-          minInterval: 25,
-          maxInterval: 50,
+          max: 200,
+          // minInterval: 25,
+          // maxInterval: 50,
         },
         yAxis: {
           type: "category",
+          axisTick: { show: false },
+          axisLine: { show: false },
+          splitArea: true,
+          color: '#fff',
+          fontWeight: 'bold'
         },
         series: [
           {
-            type: "bar",
+            type: "pictorialBar",
+            symbolRepeat: "true",
+            symbolClip: true,
+            symbolOffset: ["50%", 0],
+            // symbolPosition: 'center',
+            symbol:
+              "path://M530.295692 549.739533v-55.816237h121.553426c10.275018 0 18.605753-8.329712 18.605754-18.605753 0-10.275018-8.329712-18.605753-18.605754-18.605754H543.246655l121.759111-102.329596c7.265474-7.265474 7.265474-19.045775 0-26.311249s-19.045775-7.265474-26.312272 0L512 434.548 385.306506 328.071967c-7.265474-7.265474-19.045775-7.265474-26.312272 0-7.265474 7.265474-7.265474 19.045775 0 26.311249l121.759111 102.329597H372.150882c-10.275018 0-18.605753 8.329712-18.605754 18.605753s8.329712 18.605753 18.605754 18.605753h120.934326v55.816237h-120.934326c-10.275018 0-18.605753 8.329712-18.605754 18.605753s8.329712 18.605753 18.605754 18.605754h120.934326v93.026719c0 10.276041 8.329712 18.605753 18.605754 18.605754s18.605753-8.329712 18.605753-18.605754v-93.026719H651.850142c10.275018 0 18.605753-8.329712 18.605753-18.605754 0-10.275018-8.329712-18.605753-18.605753-18.605753l-121.55445-0.001023z m428.231131 58.784847v199.76165c0 41.102117-33.379225 74.42199-74.605162 74.42199H140.078339c-41.203424 0-74.605162-33.423227-74.605162-74.42199V608.52438c56.047504 0 101.483322-43.841507 101.483322-97.922216s-45.435819-97.922217-101.483322-97.922217V212.918297c0-41.102117 33.379225-74.42199 74.605162-74.421989h743.843322c41.203424 0 74.605162 33.423227 74.605162 74.421989v199.76165c-56.047504 0-101.483322 43.841507-101.483322 97.922217s45.435819 97.922217 101.483322 97.922216z",
             itemStyle: {
               color: (arg) => {
                 let targerColor;
                 if (arg.data.gender == "男") {
                   targerColor = "#8EB1C7";
                 } else if (arg.data.gender == "女") {
-                  targerColor = '#EA9FA1'
+                  targerColor = "#EA9FA1";
                 }
-                return targerColor
+                return targerColor;
               },
             },
             label: {
               show: true,
-              color: 'inherit',
+              color: "inherit",
               formatter: (msg) => {
                 // console.log("msg", msg);
                 let retStr = "";
-                retStr = `第${msg.data.index}名\t作品：${msg.data.number}部\n${msg.data.work}`;
+                retStr = `第${msg.data.index}名\t\t票房：${msg.data.value}亿\t\t作品：${msg.data.number}部\n${msg.data.work}`;
                 return retStr;
               },
               position: "right",
             },
           },
         ],
-        grid: {
-          top: "10%",
-          left: "6%",
-          right: "30%",
-          bottom: "6%",
-          containLabel: true,
-        },
       };
       this.chartInstance.setOption(initOption);
     },
@@ -104,8 +114,8 @@ export default {
       this.upData();
     },
     upData() {
-      // let showArr = this.allData.slice(this.startMovie, this.endMovie);
-      let showArr = this.allData.slice(0, 10);
+      let showArr = this.allData.slice(this.startMovie, this.endMovie);
+      // let showArr = this.allData.slice(0, 10);
       let nameArr = showArr.map((actor) => {
         return actor.Name;
       });
@@ -121,10 +131,6 @@ export default {
       const upDataOption = {
         series: {
           data: officeArr,
-          label: {},
-          rich: {
-            name: {},
-          },
         },
         yAxis: {
           data: nameArr,
@@ -133,13 +139,12 @@ export default {
       this.chartInstance.setOption(upDataOption);
     },
     dataInterval() {
-      setInterval(() => {
+      this.timerId = setInterval(() => {
         if (this.addMovie < 10) {
           this.addMovie++;
         } else {
           if (this.endMovie > this.allData.length) {
-            this.startMovie = 0;
-            this.addMovie = 1;
+            clearInterval(this.timerId)
           } else {
             this.startMovie++;
           }
@@ -156,13 +161,6 @@ export default {
             fontSize: titleFontSize,
           },
         },
-        tooltip: {
-          axisPointer: {
-            lineStyle: {
-              width: 66,
-            },
-          },
-        },
         xAxis: {
           axisLabel: {
             fontSize: titleFontSize * scaleSize,
@@ -176,9 +174,9 @@ export default {
         series: [
           {
             barWidth: titleFontSize,
-            itemStyle: {
-              barBorderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0],
-            },
+            // itemStyle: {
+            //   barBorderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0],
+            // },
             label: {
               fontSize: titleFontSize * scaleSize,
             },
